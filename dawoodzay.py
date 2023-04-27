c48aa43a90f231e10f2e589e83703804f47f31df
Warisdawoodzay3 <131946293+Warisdawoodzay3@users.noreply.github.com>
2023-04-27 16:35:57 +0430

    Update dawoodzay.py

--- dawoodzay.py
+++ dawoodzay.py
@@ -428,7 +428,7 @@ class Main:
 			input("\n\033[1;37m TYPE THE OWNER FACEBOOK ACCOUNT NAME \033[1;37m")
 			time.sleep(3.1)
 
-            print(" Select Correctly ")
+            print("Select Correctly")
 			time.sleep(1)
 			Main()
 			
